adc7947a2fc1fb76619d293ac7fb353491d98a1f
Morco <morco.mail@gmail.com>
2017-04-08 14:32:32 +0300

    Added plane and sphere mesh

--- Deploy/SampleProject/Assets/Meshes/Default/QuadMesh.lua
+++ Deploy/SampleProject/Assets/Meshes/Default/QuadMesh.lua
@@ -1,10 +1,10 @@
 -- add vertices
 MESH:ClearVertices()
 
-MESH:AddVertex( Vertex( Vector3(-0.5,-0.5,0), Vector4(1,0,0,1), Vector3(0,0,1), Vector2(0, 1), Vector3(0,0,0), Vector3(0,0,0) ) )             
-MESH:AddVertex( Vertex( Vector3( 0.5,-0.5,0), Vector4(0,1,0,1), Vector3(0,0,1), Vector2(1, 1), Vector3(0,0,0), Vector3(0,0,0) ) ) 
-MESH:AddVertex( Vertex( Vector3( 0.5, 0.5,0), Vector4(0,0,1,1), Vector3(0,0,1), Vector2(1, 0), Vector3(0,0,0), Vector3(0,0,0) ) ) 
-MESH:AddVertex( Vertex( Vector3(-0.5, 0.5,0), Vector4(1,0,0,1), Vector3(0,0,1), Vector2(0, 0), Vector3(0,0,0), Vector3(0,0,0) ) )
+MESH:AddVertex( Vertex( Vector3(-0.5, -0.5, 0), Vector4(1,0,0,1), Vector3(0,0,1), Vector2(0, 1), Vector3(0,0,0), Vector3(0,0,0) ) )             
+MESH:AddVertex( Vertex( Vector3( 0.5, -0.5, 0), Vector4(0,1,0,1), Vector3(0,0,1), Vector2(1, 1), Vector3(0,0,0), Vector3(0,0,0) ) ) 
+MESH:AddVertex( Vertex( Vector3( 0.5,  0.5, 0), Vector4(0,0,1,1), Vector3(0,0,1), Vector2(1, 0), Vector3(0,0,0), Vector3(0,0,0) ) ) 
+MESH:AddVertex( Vertex( Vector3(-0.5,  0.5, 0), Vector4(1,0,0,1), Vector3(0,0,1), Vector2(0, 0), Vector3(0,0,0), Vector3(0,0,0) ) )
 
 
 
